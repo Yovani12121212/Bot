2215f70d4cf033e6aaf6e5d32fa499ca550cc846
⛥TR0Y4NW0LF⛥ <63571538+lobito-kun@users.noreply.github.com>
2022-05-08 08:17:14 -0300

    Update estado.js

--- plugins/estado.js
+++ plugins/estado.js
@@ -2,11 +2,11 @@ let handler = async (m, { conn, command, usedPrefix }) => {
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let mentionedJid = [who]
 conn.send3Button(m.chat, `
-*ミ💖 Hola estimado usuario 💖彡*
+*⭐ Hola Max ⭐*
 
-*ミ🤖 Estado del Bot 🤖彡*
-*=> Bot activo ✔️*
-*=> Bot uso público ✔️*
-`.trim(), '©The Shadow Borkers - Bot', '🔷️ MENÚ 🔷', `${usedPrefix}menu`, '🔶️ MENÚ SIMPLE 🔶️', `${usedPrefix}menusimple`, '💠️ MENÚ AUDIOS 💠️', `${usedPrefix}menuaudios`, m, false, { contextInfo: { mentionedJid }})}
+*ミ🤖 Estado de S.K.Y 🤖彡*
+*=> NixiSystem activo 🌼*
+*=> SKY activo /-/ ✔️*
+`.trim(), '©S.K.Y by Mx', '🔷️ MENÚ 🔷', `${usedPrefix}menu`, '🔶️ MENÚ SIMPLE 🔶️', `${usedPrefix}menusimple`, '💠️ MENÚ AUDIOS 💠️', `${usedPrefix}menuaudios`, m, false, { contextInfo: { mentionedJid }})}
 handler.command = /^(estado|status|estate|state|stado|stats)$/i
 module.exports = handler
